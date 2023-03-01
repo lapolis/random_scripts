ac8f2157f0a08e9734c17ea8a21e7fcdb1164617
lapolis_aka_blu <57040859+lapolis@users.noreply.github.com>
2023-03-01 08:19:42 +0000

    Create badger_quick.sh

---
+++ badger_quick.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PP=$1
+NAME=$(echo $PP | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
+OUTDIR="$(echo $PP | rev | cut -d'/' -f2- | rev)/compiled"
+mkdir $OUTDIR 2>/dev/null
+echo Getting shellcode from $PP
+echo Naming payload $NAME
+echo Output dir is $OUTDIR
+set -e
+cp $PP badger.bin
+echo '#include <windows.h>' > shellcode.h
+echo '' >> shellcode.h
+xxd -i badger.bin >> shellcode.h
+make
+mv shellcode.dll $OUTDIR/$NAME.dll
+mv shellcode.exe $OUTDIR/$NAME.exe
+echo DONE
