53e25a9f8d18386c43a3b51f3d7b9ca6d62dbb45
lapolis_aka_blu <57040859+lapolis@users.noreply.github.com>
2023-09-05 17:14:07 +0100

    Create get_hosts.sh

---
+++ get_hosts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ./get_hosts.sh 192.168.0.0/24
+
+SS="$1"
+FF=`echo $SS | tr '/' '-'`
+sudo nmap -p22,139,445,80,443 -T5 $SS -oG "$FF"_nmap_NP -Pn -vv
+sudo nmap -v -sn $SS -T5 -oN "$FF"_nmap_PS -vv
+grep -i "host is up" -B1 "$FF"_nmap_PS.nmap | grep report | rev | cut -d' ' -f1 | tr -d '()' | rev >> "$FF"_up.ip.tmp
+grep /open/ "$FF"_nmap_NP.gnmap | cut -d' ' -f2 >> "$FF"_up.ip.tmp
+cat "$FF"_up.ip.tmp | sort -u > "$FF"_up.ip
